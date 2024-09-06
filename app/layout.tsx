e5cccd8068bd68faa722a63f57afcc8efdb7c57b
realfrankiewilson <frankuwill101@gmail.com>
2024-09-06 18:56:53 +0100

    Feat: Finish setting up dev environment, utils file and dependecis

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+// import localFont from "next/font/local";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-ibm-plex-serif'
+})
+
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "BankX",
+  description: "One platform, infinit possibilities",
+  icons: {
+    icon: '/icons/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${inter.variable} ${ibmPlexSerif.variable}`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
